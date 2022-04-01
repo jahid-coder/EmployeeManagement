4c5f75b94b5ef739eb0ef54511cb5fb36cfa80b1
jahidul Islam <jahidiu@gmail.com>
2022-04-01 14:00:28 +0600

    employeemangementApp

--- EmployeeManagementApp/Startup.cs
+++ EmployeeManagementApp/Startup.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using System;
 using System.Collections.Generic;
@@ -11,6 +12,13 @@ namespace EmployeeManagementApp
 {
     public class Startup
     {
+        private IConfiguration _config;
+
+        public Startup(IConfiguration config)
+        {
+            _config = config;
+        }
+
         // This method gets called by the runtime. Use this method to add services to the container.
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
@@ -27,7 +35,7 @@ namespace EmployeeManagementApp
 
             app.Run(async (context) =>
             {
-                await context.Response.WriteAsync("Hello World!");
+                await context.Response.WriteAsync(_config["MyKey"]);
             });
         }
     }
