08beabde3471df6a074f028906ac1b1374952938
jahidul Islam <jahidiu@gmail.com>
2022-04-06 16:58:10 +0600

    employeemangementApp

---
+++ EmployeeManagementApp/Models/MockEmployeeRepository.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace EmployeeManagementApp.Models
+{
+    public class MockEmployeeRepository : IEmployeeRepository
+    {
+        private List<Employee> _employeeList;
+        public MockEmployeeRepository()
+        {
+            _employeeList = new List<Employee>()
+            {
+                new Employee(){Id = 1, Name="Mary", Department="HR",Email="m@gmail.com" },
+                new Employee(){Id = 2, Name="John", Department="IT",Email="j@gmail.com"},
+                new Employee(){Id = 3, Name="Sam", Department="IT",Email="s@gmail.com"},
+            };
+        }
+
+        public Employee GetEmployee(int Id)
+        {
+            return _employeeList.FirstOrDefault(e => e.Id == Id);
+        }
+    }
+}
--- EmployeeManagementApp/Startup.cs
+++ EmployeeManagementApp/Startup.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Builder;
+﻿using EmployeeManagementApp.Models;
+using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.Configuration;
@@ -24,30 +25,27 @@ namespace EmployeeManagementApp
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+           services.AddMvc();
+            services.AddSingleton<IEmployeeRepository, MockEmployeeRepository>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IHostingEnvironment env,
-            ILogger<Startup>logger)
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
         {
             if (env.IsDevelopment())
             {
-                DeveloperExceptionPageOptions developerExceptionPageOptions = new DeveloperExceptionPageOptions()
-                {
-                    SourceCodeLineCount = 1
-                };
-
-                app.UseDeveloperExceptionPage(developerExceptionPageOptions);
+               
+                app.UseDeveloperExceptionPage();
             }
 
          
-            app.UseFileServer();
-           
-            app.Run(async (context) =>
-            {
-                throw new Exception("some error processing the request");
-                await context.Response.WriteAsync("Hello world");
-            });
+            app.UseStaticFiles();
+            app.UseMvcWithDefaultRoute();
+
+            //app.Run(async (context) =>
+            //{
+            //    await context.Response.WriteAsync("Hello World");
+            //});
         }
     }
 }
